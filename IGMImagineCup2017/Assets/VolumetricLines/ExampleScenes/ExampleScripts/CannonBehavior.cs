d14e5115e296b0ba66a3b64b72785cdb84fcde30
MAIN\asb9599 <asb9599@g.rit.edu>
2017-10-21 17:37:55 -0400

    Added basic mouse aiming to a ship cannon

--- IGMImagineCup2017/Assets/VolumetricLines/ExampleScenes/ExampleScripts/CannonBehavior.cs
+++ IGMImagineCup2017/Assets/VolumetricLines/ExampleScenes/ExampleScripts/CannonBehavior.cs
@@ -6,17 +6,25 @@ public class CannonBehavior : MonoBehaviour {
 	public Transform m_cannonRot;
 	public Transform m_muzzle;
 	public GameObject m_shotPrefab;
-	public Texture2D m_guiTexture;
+
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+    
+
 
 	// Use this for initialization
 	void Start () 
 	{
 	
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        m_cannonRot.transform.Rotate(v, h, 0);
+        /*
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			m_cannonRot.transform.Rotate(Vector3.up, -Time.deltaTime * 100f);
@@ -25,15 +33,14 @@ public class CannonBehavior : MonoBehaviour {
 		{
 			m_cannonRot.transform.Rotate(Vector3.up, Time.deltaTime * 100f);
 		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
-			GameObject.Destroy(go, 3f);
-		}
+        */
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+            GameObject.Destroy(go, 3f);
+        }
+    }
+
 	}
 
-	void OnGUI()
-	{
-		GUI.DrawTexture(new Rect(0f, 0f, m_guiTexture.width / 2, m_guiTexture.height / 2), m_guiTexture);
-	}
-}
+
