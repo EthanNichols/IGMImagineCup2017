e6fc4cb054562e93e28c0ea03f08487fcdec2262
Chase Slattery (RIT Student) <chs6298@ad.rit.edu>
2017-10-20 23:44:25 -0400

    Added code files

---
+++ IGMImagineCup2017/Assets/Scripts/Crate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+    private GameObject terrain;
+	// Use this for initialization
+	void Start ()
+    {
+        terrain = GameObject.FindGameObjectWithTag("Terrain");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
