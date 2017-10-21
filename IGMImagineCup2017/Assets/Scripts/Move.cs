be1818622bf7b7f70a46c28eaf7958da6b79a109
MAIN\etn6701 <ethan.thomas.nichols@gmail.com>
2017-10-21 10:12:16 -0400

    Created a boat prefab for other people Finished the movement script

--- IGMImagineCup2017/Assets/Scripts/Move.cs
+++ IGMImagineCup2017/Assets/Scripts/Move.cs
@@ -4,27 +4,64 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
+    //The speed and max speed of the boat
+    public float speed;
+    public float maxSpeed;
+
+    //The roation speed for the boat, and the current roation amount
+    public float rotationSpeed;
+    private float rotation;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.W)) {
-            transform.position += new Vector3(1, 0, 0);
-        } else if (Input.GetKey(KeyCode.S))
+        //Accelerate the boat when pressing forward, else decellerate the boat
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position -= new Vector3(1, 0, 0);
+            Accelerate();
         }
-        else if (Input.GetKey(KeyCode.A))
+        else
         {
-            transform.position += new Vector3(0, 0, 1);
+            Decellerate();
+        }
+
+        //Rotate the boat based on the key input
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotate(1);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position -= new Vector3(0, 0, 1);
+            Rotate(-1);
         }
+    }
 
+    private void Rotate(int rotateDirection)
+    {
+        //Calculate the new rotation amount
+        //Display the new rotation amount
+        rotation += rotationSpeed * rotateDirection;
+        transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+    }
+
+    private void Accelerate()
+    {
+        //Apply instant velocity to the ship
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        //Cap the max speed for the boat
+        Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxSpeed);
+    }
+
+    private void Decellerate()
+    {
+        //Slow the boat down relative to the velocity
+        GetComponent<Rigidbody>().velocity *= .99f;
+
+        //Set the velocity to 0 if it is significantly small
+        if (GetComponent<Rigidbody>().velocity.magnitude < .2f) { GetComponent<Rigidbody>().velocity = Vector3.zero; }
     }
 }
